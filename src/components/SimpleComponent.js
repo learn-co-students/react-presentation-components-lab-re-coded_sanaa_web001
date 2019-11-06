learn-co-students/react-presentation-components-lab-noukod-000

Add solution code

src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponentHere Here
+import React, { Component } from 'react';
+
+class DumbComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mood: 'happy'
+    };
+  }
+
+  handleClick = () => {
+    const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    this.setState({ mood: newMood });
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
+
+export default DumbComponent;

learn-co-students/react-presentation-components-lab-re-coded_sanaa_web001

src/components/SimpleComponent.js
@@ -1,1 +1,22 @@
-// Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mood: 'happy'
+    };
+  }
+
+  handleClick = () => {
+    const newMood = this.state.mood === 'happy' ? 'sad' : 'happy';
+    this.setState({ mood: newMood });
+  }
+
+  render() {
+    return <div onClick={this.handleClick}>{this.state.mood}</div>;
+  }
+}
+
+export default SimpleComponent;