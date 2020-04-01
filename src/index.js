a543a2e886b705c3904914e740ef4da4e85c3d8a
Liz Burton <liz.burton147@gmail.com>
2020-04-01 11:20:52 -0400

    Added solution code to index.js
    Current solution code passes the tests but doesn't match the lab instructions

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={alert("clicked!")} />
   </div>,
   document.getElementById('root')
 );
