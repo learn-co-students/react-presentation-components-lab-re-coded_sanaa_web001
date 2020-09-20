3110e7ff65a7045cc5e96085a20538b1a0a0b2c5
Nusiba Ibrahim <ainusiba@gmail.com>
2020-09-20 07:32:08 +0000

    Done.

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,28 @@
 // Code SimpleComponent Here
+import React, { Component } from 'react';
+
+class SimpleComponent extends Component {
+
+    state = {
+        mood: "happy"
+    }
+
+    handleClick = () => {
+        let newMood
+
+        this.state.mood === "happy" ? newMood = "sad" : newMood = "happy"
+
+        this.setState({
+            mood: newMood
+        })
+    }
+
+    render() {
+        return (
+            <div onClick={this.handleClick}>
+                {this.state.mood}
+            </div>
+        )
+    }
+}
+export default SimpleComponent
--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+const changeColor = (event) => {
+  event.target.style.color === "red" ? event.target.style.color = "black" : event.target.style.color = "red"
+}
+
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={changeColor} />
   </div>,
   document.getElementById('root')
 );
