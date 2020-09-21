c3e04f17d579827a28d1a1d2102e345bc003360c
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-21 14:22:51 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={()=>{alert('I\'m clicked ')}} />
   </div>,
   document.getElementById('root')
 );
