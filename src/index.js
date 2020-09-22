cbc8d4adaaeef670e5d2150c7ac8fedf2131da00
Somaia Abdullah <somia.abdullah3@gmail.com>
2020-09-22 12:30:48 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+  let clickHandle = () => console.log('Clicked')
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={clickHandle} />
   </div>,
   document.getElementById('root')
 );
